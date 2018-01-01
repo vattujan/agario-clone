edb1ff8a7b1e431d6216e2714293eeb232bc41ee
Vattujan <yoge0030@edu.easj.dk>
2018-01-01 17:39:35 +0100

    SimpleFix

--- FinalGame/scripts/main.js
+++ FinalGame/scripts/main.js
@@ -1,4 +1,4 @@
-// Create a new Phaser game object with a single state that has 3 functions
+// Create a new Phaser game object with a single state that has 4 functions
 var game = new Phaser.Game(800, 800, Phaser.AUTO, 'phaser', {
     preload: preload,
     create: create,
